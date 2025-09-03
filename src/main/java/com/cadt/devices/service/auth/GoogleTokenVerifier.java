17a71ff737bb9c8de18857c066828f224af625a3
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-09-04 01:02:27 +0530

    refactor authentication DTOs to use Lombok for boilerplate code and add validation annotations

--- src/main/java/com/cadt/devices/service/auth/GoogleTokenVerifier.java
+++ src/main/java/com/cadt/devices/service/auth/GoogleTokenVerifier.java
@@ -1,4 +1,31 @@
 package com.cadt.devices.service.auth;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
 public class GoogleTokenVerifier {
+
+    private final GoogleIdTokenVerifier verifier;
+
+    public GoogleTokenVerifier(@Value("${google.oauth.client-id}") String clientId) {
+        this.verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(),
+                GsonFactory.getDefaultInstance()).setAudience(Collections.singletonList(clientId)).build();
+    }
+
+    public Payload verify(String idToken) {
+        try {
+            GoogleIdToken t = verifier.verify(idToken);
+            return t != null ? t.getPayload() : null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
