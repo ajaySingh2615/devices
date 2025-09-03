6e8abf17751263bf3874267f0d65b626ab90b5df
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-09-03 21:01:16 +0530

    refactor RefreshTokenRepository to extend JpaRepository and add custom query methods

--- src/main/java/com/cadt/devices/repo/tokens/RefreshTokenRepository.java
+++ src/main/java/com/cadt/devices/repo/tokens/RefreshTokenRepository.java
@@ -1,4 +1,14 @@
 package com.cadt.devices.repo.tokens;
 
-public class RefreshTokenRepository {
+import com.cadt.devices.model.token.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.Instant;
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findByTokenHash(String tokenHash);
+
+    void deleteByExpiresAtBefore(Instant time);
 }
