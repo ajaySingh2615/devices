28d77d5efb59f4503f4a13af078280f1478e531e
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-09-03 21:21:58 +0530

    refactor authentication DTOs to use Lombok for boilerplate code and add validation annotations

--- src/main/java/com/cadt/devices/dto/user/UpdateProfileRequest.java
+++ src/main/java/com/cadt/devices/dto/user/UpdateProfileRequest.java
@@ -1,4 +1,15 @@
 package com.cadt.devices.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UpdateProfileRequest {
+
+    @NotBlank
+    private String name;
+    private String phone;
+    private String avatarUrl;
 }
--- src/main/java/com/cadt/devices/dto/user/UserResponse.java
+++ src/main/java/com/cadt/devices/dto/user/UserResponse.java
@@ -1,4 +1,18 @@
 package com.cadt.devices.dto.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class UserResponse {
+
+    private String id;
+    private String name;
+    private String email;
+    private String phone;
+    private String role;
+    private String avatarUrl;
 }
