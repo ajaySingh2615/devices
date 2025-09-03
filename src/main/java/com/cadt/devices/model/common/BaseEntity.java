3654a025053c68af3af6847978d58de9c069fe14
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-09-03 20:20:28 +0530

    refactor exception handling to enhance ApiException and introduce ErrorResponse as a record

--- src/main/java/com/cadt/devices/model/common/BaseEntity.java
+++ src/main/java/com/cadt/devices/model/common/BaseEntity.java
@@ -1,4 +1,26 @@
 package com.cadt.devices.model.common;
 
-public class BaseEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.validator.constraints.UUID;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.Instant;
+
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @Column(columnDefinition = "CHAR(36)")
+    private String id;
+
+    @CreatedDate @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate @Column(nullable = false)
+    private Instant updatedAt;
 }
