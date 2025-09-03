27f3830245dd06a70f5f2694dc0434de61018480
ajaySingh2615 <ajaysingh261526@gmail.com>
2025-09-03 20:25:32 +0530

    refactor BaseEntity to use Lombok for getters/setters and add auditing support

--- src/main/java/com/cadt/devices/model/common/BaseEntity.java
+++ src/main/java/com/cadt/devices/model/common/BaseEntity.java
@@ -4,12 +4,20 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import org.hibernate.annotations.UuidGenerator;
-import org.hibernate.validator.constraints.UUID;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.Instant;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
 public abstract class BaseEntity {
 
     @Id
@@ -18,9 +26,11 @@ public abstract class BaseEntity {
     @Column(columnDefinition = "CHAR(36)")
     private String id;
 
-    @CreatedDate @Column(nullable = false, updatable = false)
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
     private Instant createdAt;
 
-    @LastModifiedDate @Column(nullable = false)
+    @LastModifiedDate
+    @Column(nullable = false)
     private Instant updatedAt;
 }
